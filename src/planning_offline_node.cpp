d749ff80694ce62c326bef27259ba27a4129c5df
marios-stam <mariosstamatopoulos1@gmail.com>
2022-05-18 20:35:23 +0200

    Made 2 separate nodes fon realtime and offfline planning

---
+++ src/planning_offline_node.cpp
@@ -0,0 +1,55 @@
+/*
+ * ompl_example_2d_node.cpp
+ *
+ *  Created on: April 6, 2020
+ *      Author: Dominik Belter
+ *   Institute: Instute of Robotics and Machine Intelligence, Poznan University of Technology
+ */
+
+#include "../include/ompl_example_2d/fcl_checker_realtime.hpp"
+
+#include "../include/ompl_example_2d/ompl_example_2d.hpp"
+
+#include "../include/catenaries/catenary.hpp"
+#include "../include/custom_mesh.hpp"
+
+#include "../include/moving_obstacles.hpp"
+
+#include <ros/ros.h>
+// include transform listener
+#include <tf/transform_listener.h>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Path.h>
+
+// custom services
+#include <drones_rope_planning/CylinderObstacleData.h>
+#include <drones_rope_planning/PlanningRequest.h>
+#include <drones_rope_planning/rigid_body_dynamic_path.h>
+
+namespace ob = ompl::base;
+namespace og = ompl::geometric;
+
+int main(int argc, char **argv)
+{
+    // init ROS node
+    ros::init(argc, argv, "ompl_example_2d");
+    // create node handler
+    ros::NodeHandle nodeHandle("~");
+    //=======================================================================================================================
+    // get problem parameters
+    problem_params::ProblemParams prob_prms = problem_params::getProblemParams(nodeHandle);
+    ompl_rope_planning::planner planner(prob_prms);
+    printf("Setting start and goal\n");
+    float L = prob_prms.L;
+    float start[6] = {prob_prms.start_pos["x"], prob_prms.start_pos["y"], prob_prms.start_pos["z"], 0.0, L * 0.5, 0.0};
+    float goal[6] = {prob_prms.goal_pos["x"], prob_prms.goal_pos["y"], prob_prms.goal_pos["z"], 0.0, L * 0.5, 0.0};
+    planner.setStartGoal(start, goal);
+    printf("Planning...\n");
+    planner.plan();
+    printf("Done!\n");
+    // exit ros node
+    ros::shutdown();
+    return 0;
+}
